b26fc7012afc4ca56cca9ba308ddade67f4684fc
Rupali <rupalidoke97@gmail.com>
2022-12-29 14:20:21 +0530

    minor changes

--- src/utils/getCollateralPrices.ts
+++ src/utils/getCollateralPrices.ts
@@ -28,30 +28,35 @@ export type ICollateralPrices = {
 };
 
 export const getCollateralPrices = async (): Promise<ICollateralPrices> => {
-  const result = await CoinGeckoClient.simple.price({
-    ids: "bitcoin,ethereum,dai,tether,mahadao,arth,usd-coin,scallop,binance-usd,apeswap-finance,wmatic,wbnb,frax-share,binancecoin",
-    vs_currencies: "USD",
-  });
+  let result
+  try {
+     result = await CoinGeckoClient.simple.price({
+      ids: "bitcoin,ethereum,dai,tether,mahadao,arth,usd-coin,scallop,binance-usd,apeswap-finance,wmatic,wbnb,frax-share,binancecoin",
+      vs_currencies: "USD",
+    });
+  } catch (error) {
+    console.log("getCollateralPrices error", error)
+  }
 
   return {
-    ARTH: result.data.arth.usd,
-    WBTC: result.data.bitcoin.usd,
-    BUSD: result.data["binance-usd"].usd,
-    USDT: result.data.tether.usd,
-    "polygon.3pool": 1.048,
-    "ARTH.usd": 1,
-    "bsc.3eps": 1.0392,
-    DAI: result.data.dai.usd,
-    USDC: result.data["usd-coin"].usd,
-    WETH: result.data.ethereum.usd,
-    MAHA: result.data.mahadao.usd,
-    SCLP: result.data.scallop.usd,
-    BANNANA: result.data["apeswap-finance"].usd,
-    BSCUSDC: result.data["usd-coin"].usd,
-    BSCUSDT: result.data.tether.usd,
-    WMATIC: result.data.wmatic.usd,
-    WBNB: result.data.wbnb.usd,
-    FRAX: result.data['frax-share'].usd,
-    BNB: result.data['binancecoin'].usd
+    ARTH: result?.data.arth.usd || 0,
+    WBTC: result?.data.bitcoin.usd || 0,
+    BUSD: result?.data["binance-usd"].usd || 0,
+    USDT: result?.data.tether.usd || 0,
+    "polygon.3pool": 1.048 || 0,
+    "ARTH.usd": 1 || 0,
+    "bsc.3eps": 1.0392 || 0,
+    DAI: result?.data.dai.usd || 0,
+    USDC: result?.data["usd-coin"].usd || 0,
+    WETH: result?.data.ethereum.usd || 0,
+    MAHA: result?.data.mahadao.usd || 0,
+    SCLP: result?.data.scallop.usd || 0,
+    BANNANA: result?.data["apeswap-finance"].usd || 0,
+    BSCUSDC: result?.data["usd-coin"].usd || 0,
+    BSCUSDT: result?.data.tether.usd || 0,
+    WMATIC: result?.data.wmatic.usd || 0,
+    WBNB: result?.data.wbnb.usd || 0,
+    FRAX: result?.data['frax-share'].usd || 0,
+    BNB: result?.data['binancecoin'].usd || 0
   };
 };
