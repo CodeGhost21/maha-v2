a189fe2a115f0ee66949d36f0d76b6a9a1befc3f
Aniketh Panchidi <ani.p1995@gmail.com>
2022-12-29 13:31:05 +0530

    minor chaneg in script file

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const nconf = require("nconf");
 
+nconf.argv()
+  .env()
+  .file({ file: './config.json' })
 
 process.env.ROOT_PATH = __dirname;
 
-console.log("here", process.env.NODE_ENV === "production")
+console.log("here", nconf.get("NODE_ENV") === "production")
 if (nconf.get("NODE_ENV") === "production" || process.env.NODE_ENV === "production") {
   require("./dist/index");
 } else {
+  console.log('else');
   require("./src/index");
 }
