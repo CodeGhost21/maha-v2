ef5a9af913461aa6bca500a90ea7eef1f8090791
Steven Enamakel <enamakel@mahadao.com>
2022-12-30 00:17:45 +0530

    removed unwanted prices

--- src/utils/getCollateralPrices.ts
+++ src/utils/getCollateralPrices.ts
@@ -2,26 +2,7 @@ import CoinGecko from "coingecko-api";
 
 const CoinGeckoClient = new CoinGecko();
 
-export type CollateralKeys =
-  | "USDC"
-  | "WBTC"
-  | "BUSD"
-  | "USDT"
-  | "ARTH.usd"
-  | "bsc.3eps"
-  | "polygon.3pool"
-  | "DAI"
-  | "WETH"
-  | "MAHA"
-  | "ARTH"
-  | "SCLP"
-  | "BANNANA"
-  | "BSCUSDC"
-  | "BSCUSDT"
-  | "WMATIC"
-  | "WBNB"
-  | "FRAX"
-  | "BNB";
+export type CollateralKeys = "MAHA" | "SCLP" | "BNB";
 
 export type ICollateralPrices = {
   [key in CollateralKeys]: number;
@@ -39,24 +20,8 @@ export const getCollateralPrices = async (): Promise<ICollateralPrices> => {
   }
 
   return {
-    ARTH: result?.data.arth.usd || 0,
-    WBTC: result?.data.bitcoin.usd || 0,
-    BUSD: result?.data["binance-usd"].usd || 0,
-    USDT: result?.data.tether.usd || 0,
-    "polygon.3pool": 1.048 || 0,
-    "ARTH.usd": 1 || 0,
-    "bsc.3eps": 1.0392 || 0,
-    DAI: result?.data.dai.usd || 0,
-    USDC: result?.data["usd-coin"].usd || 0,
-    WETH: result?.data.ethereum.usd || 0,
     MAHA: result?.data.mahadao.usd || 0,
     SCLP: result?.data.scallop.usd || 0,
-    BANNANA: result?.data["apeswap-finance"].usd || 0,
-    BSCUSDC: result?.data["usd-coin"].usd || 0,
-    BSCUSDT: result?.data.tether.usd || 0,
-    WMATIC: result?.data.wmatic.usd || 0,
-    WBNB: result?.data.wbnb.usd || 0,
-    FRAX: result?.data["frax-share"].usd || 0,
     BNB: result?.data["binancecoin"].usd || 0,
   };
 };
