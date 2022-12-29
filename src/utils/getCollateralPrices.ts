f7a947f7003b09c2bfa507fb85ad9f2f5fc6d8e4
Steven Enamakel <enamakel@mahadao.com>
2022-12-30 00:21:06 +0530

    fixed price issues

--- src/utils/getCollateralPrices.ts
+++ src/utils/getCollateralPrices.ts
@@ -2,7 +2,7 @@ import CoinGecko from "coingecko-api";
 
 const CoinGeckoClient = new CoinGecko();
 
-export type CollateralKeys = "MAHA" | "SCLP" | "BNB";
+export type CollateralKeys = "MAHA";
 
 export type ICollateralPrices = {
   [key in CollateralKeys]: number;
@@ -12,16 +12,16 @@ export const getCollateralPrices = async (): Promise<ICollateralPrices> => {
   let result;
   try {
     result = await CoinGeckoClient.simple.price({
-      ids: "bitcoin,ethereum,dai,tether,mahadao,arth,usd-coin,scallop,binance-usd,apeswap-finance,wmatic,wbnb,frax-share,binancecoin",
+      ids: "mahadao",
       vs_currencies: "USD",
     });
   } catch (error) {
     console.log("getCollateralPrices error", error);
   }
 
+  console.log(result);
+
   return {
     MAHA: result?.data.mahadao.usd || 0,
-    SCLP: result?.data.scallop.usd || 0,
-    BNB: result?.data["binancecoin"].usd || 0,
   };
 };
