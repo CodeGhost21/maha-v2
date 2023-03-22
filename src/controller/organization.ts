9ccd71249431251d1cde24a40d561f4aa658f5ca
Aniketh Panchidi <ani.p1995@gmail.com>
2023-03-22 11:35:04 +0530

    minor change

--- src/controller/organization.ts
+++ src/controller/organization.ts
@@ -5,17 +5,15 @@ export const addOrganization = async (req: Request, res: Response) => {
   const checkOrganization = await Organization.findOne({
     $or: [{ name: req.body.orgName }, { guildId: req.body.guildId }],
   });
-  console.log(checkOrganization);
-
   if (!checkOrganization) {
     const newOrganization = new Organization({
       name: req.body.orgName,
       guildId: req.body.guildId,
       maxBoost: req.body.maxBoost,
     });
-
     await newOrganization.save();
     res.send(newOrganization);
+  } else {
+    res.send("already added");
   }
-  res.send("already added");
 };
