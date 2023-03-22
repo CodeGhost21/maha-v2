b46699047c5ae443dd35f60a39e2ab635599386a
Aniketh Panchidi <ani.p1995@gmail.com>
2023-03-22 12:16:07 +0530

    user loyalty removed

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,5 +1,13 @@
 import nconf from "nconf";
-import { Client, Intents, TextChannel, MessageOptions, MessageEmbed, MessageActionRow, MessageButton } from "discord.js";
+import {
+  Client,
+  Intents,
+  TextChannel,
+  MessageOptions,
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton,
+} from "discord.js";
 import { User } from "../database/models/user";
 import * as jwt from "jsonwebtoken";
 import urlJoin from "./urlJoin";
@@ -19,7 +27,7 @@ const total_icons = [
   "❌",
   "✅",
   "🔥",
-  "✋"
+  "✋",
 ];
 let commands;
 
@@ -38,10 +46,9 @@ export const client = new Client({
   ],
 });
 
-
 client.once("ready", () => {
-  console.log(`DISCORD: Logged in as ${client.user?.tag}!`)
-  const guild = client.guilds.cache.get(nconf.get('DISCORD_GUILD_ID'))
+  console.log(`DISCORD: Logged in as ${client.user?.tag}!`);
+  const guild = client.guilds.cache.get(nconf.get("DISCORD_GUILD_ID"));
 
   if (guild) {
     commands = guild.commands;
@@ -50,61 +57,68 @@ client.once("ready", () => {
   }
 
   commands?.create({
-    name: 'profile',
-    description: 'Shows your profile for Gift of Eden',
-  })
+    name: "profile",
+    description: "Shows your profile for Gift of Eden",
+  });
   commands?.create({
-    name: 'tasks',
-    description: 'Shows your loyalty tasks status',
-  })
+    name: "tasks",
+    description: "Shows your loyalty tasks status",
+  });
   commands?.create({
-    name: 'verify',
-    description: 'Verify you twitter and wallet',
-  })
-}
-)
+    name: "verify",
+    description: "Verify you twitter and wallet",
+  });
+});
 
 // Required to clean this code and make it work
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName, options } = interaction;
 
-  if (commandName === 'profile') {
+  if (commandName === "profile") {
     await User.findOne({ userID: interaction.user.id }).then(async (user) => {
       if (user) {
-        const userLoyalty = await user.getLoyalty();
+        // const userLoyalty = await user.getLoyalty();
 
         await interaction.reply({
-          content: `Hello ${user.discordName}${total_icons[13]} \n\n` +
+          content:
+            `Hello ${user.discordName}${total_icons[13]} \n\n` +
             `Total points earned: ${user.totalPoints}\n` +
-            `Current Loyalty Completed: ${userLoyalty.totalLoyalty}%\n\n` +
+            // `Current Loyalty Completed: ${userLoyalty.totalLoyalty}%\n\n` +
             `Your current GM rank: ${user.gmRank}${total_icons[3]}\n` +
             `Total number of GM said: ${user.totalGMs}\n` +
             `Highest GM Streak Record: ${user.maxStreak}\n` +
-            `Twitter Verify: ${user.signTwitter ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
-            `Wallet Connected: ${user.walletAddress ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n`,
-        })
+            `Twitter Verify: ${
+              user.signTwitter
+                ? `Completed!!${total_icons[11]}`
+                : `Pending${total_icons[10]}`
+            }\n` +
+            `Wallet Connected: ${
+              user.walletAddress
+                ? `Completed!!${total_icons[11]}`
+                : `Pending${total_icons[10]}`
+            }\n`,
+        });
       }
-    })
-  } else if (commandName === 'tasks') {
+    });
+  } else if (commandName === "tasks") {
     await User.findOne({ userID: interaction.user.id }).then(async (user) => {
-      const userLoyalty = await user?.getLoyalty();
+      // const userLoyalty = await user?.getLoyalty();
 
       await interaction.reply({
-        content: `Loyalty Tasks${total_icons[5]} \n\n` +
-          `Say GM: ${userLoyalty?.gm ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
-          `Change Twitter Profile: ${userLoyalty?.twitterProfile ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
-          `Change Discord Profile: ${userLoyalty?.discordProfile ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
-          `Revoke Opensea Access: ${userLoyalty?.opensea ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n`,
-        ephemeral: true
+        // content: `Loyalty Tasks${total_icons[5]} \n\n` +
+        //   `Say GM: ${userLoyalty?.gm ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
+        //   `Change Twitter Profile: ${userLoyalty?.twitterProfile ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
+        //   `Change Discord Profile: ${userLoyalty?.discordProfile ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n` +
+        //   `Revoke Opensea Access: ${userLoyalty?.opensea ? `Completed!!${total_icons[11]}` : `Pending${total_icons[10]}`}\n`,
+        // ephemeral: true
       });
-    })
-  } else if (commandName === 'verify') {
+    });
+  } else if (commandName === "verify") {
     const expiry = Date.now() + 86400000 * 7;
 
     await User.findOne({ userID: interaction.user.id }).then(async (user) => {
-
       const token = await jwt.sign({ id: user?.id, expiry }, jwtSecret);
       const frontendUrl = urlJoin(
         nconf.get("FRONTEND_URL"),
@@ -124,48 +138,44 @@ client.on('interactionCreate', async (interaction) => {
       );
       const discordMsgEmbed = new MessageEmbed()
         .setColor("#F07D55")
-        .setDescription("Verify yourself by clicking below")
+        .setDescription("Verify yourself by clicking below");
 
       const discordSuccessEmbed = new MessageEmbed()
         .setColor("#4ffa02")
-        .setDescription("You have been successfully verified.")
-
-
+        .setDescription("You have been successfully verified.");
 
       if (!user?.signTwitter && !user?.walletAddress) {
         await interaction.reply({
           embeds: [discordMsgEmbed],
           components: [row1, row2],
-          ephemeral: true
-        })
+          ephemeral: true,
+        });
       } else if (!user?.signTwitter) {
         await interaction.reply({
           embeds: [discordMsgEmbed],
           components: [row1],
-          ephemeral: true
-        })
+          ephemeral: true,
+        });
       } else if (!user?.walletAddress) {
         await interaction.reply({
           embeds: [discordMsgEmbed],
           components: [row2],
-          ephemeral: true
-        })
+          ephemeral: true,
+        });
       } else {
         await interaction.reply({
           embeds: [discordSuccessEmbed],
-          ephemeral: true
-        })
+          ephemeral: true,
+        });
       }
-    })
-
+    });
   }
-})
+});
 
 //This listener is for user joining the guild
 // client.on('guildMemberAdd', async member => {
 //   console.log(member)
 // })
-
 
 client.login(nconf.get("DISCORD_CLIENT_TOKEN")); //login bot using token
 
