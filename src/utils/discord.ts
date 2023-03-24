18e91567f51fb966438cdbbd46059354d83c4c7e
Elvin Skaria <encorereloded@gmail.com>
2023-03-24 18:39:17 +0530

    changes: added leaderboard and tasks commands

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -18,6 +18,7 @@ import urlJoin from "./urlJoin";
 import { Organization } from "../database/models/organisation";
 import { completeLoyaltyTask } from '../controller/loyaltyTask'
 import { sendFeedDiscord } from './sendFeedDiscord';
+import { Task } from '../database/models/tasks';
 const jwtSecret = nconf.get("JWT_SECRET");
 const total_icons = [
   "🥇",
@@ -73,6 +74,16 @@ client.once("ready", () => {
     name: "verify",
     description: "Verify you twitter and wallet",
   });
+
+  commands?.create({
+    name: "tasks",
+    description: "Get your daily tasks here.",
+  });
+
+  commands?.create({
+    name: "leaderboard",
+    description: "View the leaderboard.",
+  });
 });
 
 // Required to clean this code and make it work
@@ -108,7 +119,7 @@ client.on("interactionCreate", async (interaction) => {
 
           if (!user?.signTwitter || !user?.walletAddress) {
             content = `Hello ${interaction.user}${total_icons[13]} \n\n` +
-              `***Please verify yourself by typing /verify command***\n\n` +
+              `***Seems like you have not verified😞 your Twitter and wallet. Verify yourself using /verify command and you will get perform the amazing tasks.***\n\n` +
               `Total points earned: ${user.totalPoints}\n` +
               `Current Loyalty Completed: 0%\n\n` +
               `Highest GM Streak Record: ${user.maxStreak}\n` +
@@ -125,7 +136,7 @@ client.on("interactionCreate", async (interaction) => {
               `***You have earned ${user.loyaltyWeight * 100}% loyalty which will boost your points by 10x and you have earned a total of ${user.totalPoints} points*** \n` +
               `***hey good going you have ${user.maxStreak} days of gm streak 😮, keep going*** \n\n` +
               `Your Wallet and Twitter both are verified 🥳 \n\n` +
-              `Loyalty Tasks`
+              `**Loyalty Tasks**`
 
             // `Total points earned: ${user.totalPoints}\n` +
             // `Your current GM rank: ${user.gmRank}${total_icons[3]}\n` +
@@ -166,7 +177,7 @@ client.on("interactionCreate", async (interaction) => {
               msg = `Looking fresh with that NFT profile pic!`
             }
             await sendFeedDiscord(`${collected?.user}, ${msg}`);
-            // await collected.reply({ content: `${collected?.user}, ${taskResponse}`, ephemeral: true })
+            await collected.reply({ content: `${collected?.user}, ${taskResponse}`, ephemeral: true })
           })
         }
       });
@@ -197,8 +208,8 @@ client.on("interactionCreate", async (interaction) => {
 
         const discordMsgEmbed = new MessageEmbed()
           .setColor("#F07D55")
-          .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-          .setAuthor({ name: 'Gift of Eden', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+          .setThumbnail('https://i.imgur.com/xYG5x9G.png')
+          .setAuthor({ name: 'Gift of Eden', iconURL: 'https://i.imgur.com/xYG5x9G.png', url: 'https://peopleofeden.com/' })
           .setTitle('Title here')
           .setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,");
 
@@ -219,6 +230,71 @@ client.on("interactionCreate", async (interaction) => {
           });
         }
       });
+    } else if (commandName === "tasks") {
+      const rowItem: any[] = []
+
+      const organization = await Organization.findOne({ guildId: interaction.guild?.id })
+      const allTasks = await Task.find({ organizationId: organization?.id })
+      allTasks.map((item) => {
+        rowItem.push({
+          label: item.name,
+          description: 'description',
+          value: item.type
+        })
+      })
+
+      const row = new MessageActionRow()
+        .addComponents(
+          new MessageSelectMenu()
+            .setCustomId("taskSelect")
+            .setPlaceholder("Select a task")
+            .addOptions(rowItem)
+        )
+      if (rowItem.length < 1) {
+
+        await interaction.reply({
+          content: 'No tasks have been listed yet.',
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: '**Daily Tasks**',
+          ephemeral: true,
+          components: [row]
+        });
+      }
+
+      const taskCollector = interaction.channel?.createMessageComponentCollector({
+        componentType: "SELECT_MENU",
+      });
+
+      taskCollector?.on("collect", async (collected: any) => {
+        let msg;
+        const value = collected.values[0]
+        if (value === 'gm') {
+          msg = `Go and say GM in the GM channel`
+        }
+        await collected.reply({ content: `Hey ${collected?.user}, ${msg}`, ephemeral: true })
+      })
+    } else if (commandName === "leaderboard") {
+      User.find({})
+        .lean()
+        .then((users) => {
+          const top = users
+            .sort((a, b) => b.totalPoints - a.totalPoints)
+            .slice(0, 10)
+            .map(
+              (u, i) => `${total_icons[i]} **${u.userTag}** - **${u.totalPoints}** points!`
+            )
+            .join("\n");
+
+
+          const text =
+            "__Top 10 rankers__ 🏆\n" +
+            top
+
+          interaction.reply(text);
+        });
     }
   } catch (error) {
     console.error(error);
