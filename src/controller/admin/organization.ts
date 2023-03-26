1cff43a67609d9d76f848cbed230642ef7c4a940
Steven Enamakel <enamakel@mahadao.com>
2023-03-27 01:23:29 +0300

    mionr changes

--- src/controller/admin/organization.ts
+++ src/controller/admin/organization.ts
@@ -10,11 +10,12 @@ export const updateOrg = async (req: Request, res: Response) => {
   const org = await Organization.findById(serverProfile.organizationId);
   if (!org) throw new NotFoundError("org not found");
 
-  org.name = req.body.orgName || org.name;
+  org.name = req.body.name || org.name;
   org.maxBoost = req.body.maxBoost || org.maxBoost;
   org.guildId = req.body.guildId || org.guildId;
   org.feedChannelId = req.body.feedChannelId || org.feedChannelId;
   org.msgChannelId = req.body.msgChannelId || org.msgChannelId;
+  org.gmChannelId = req.body.gmChannelId || org.gmChannelId;
 
   await org.save();
   res.json({ success: true });
