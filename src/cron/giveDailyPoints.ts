77fe85b220e7de494e9a403d284456ef6d8b2f7b
Aniketh Panchidi <ani.p1995@gmail.com>
2023-03-30 23:21:20 +0530

    minor change

--- src/cron/giveDailyPoints.ts
+++ src/cron/giveDailyPoints.ts
@@ -5,7 +5,7 @@ import { nftHoldTask } from "../controller/task/nftHold";
 import { twitterFollowTask } from "../controller/task/twitterFollow";
 
 export const dailyPoints = async () => {
-  cron.schedule("0 0 * * *", () => {
+  cron.schedule("* * * * *", () => {
     console.log("Running every Day");
     nftHoldTask();
     twitterFollowTask();
