651f1a522d703f1e5a370d0d6c2564a0de97c9a3
Elvin Skaria <encorereloded@gmail.com>
2023-03-31 15:10:10 +0530

    test command

--- src/bots/discord.ts
+++ src/bots/discord.ts
@@ -5,6 +5,8 @@ import { executeTasksCommand } from "../controller/discord/tasks";
 import { executeLeaderboardCommand } from "../controller/discord/leaderboard";
 import { executeSetupCommand } from "../controller/discord/setup";
 import { executeLoyaltyCommand } from "../controller/discord/loyalty";
+import { MessageActionRow, MessageSelectMenu } from "discord.js";
+import { MessageComponentTypes } from "discord.js/typings/enums";
 
 
 client.once("ready", () => {
@@ -41,9 +43,16 @@ client.once("ready", () => {
     name: "setup",
     description: "Setup your server (Server owners only)",
   });
+
+  commands?.create({
+    name: "test",
+    description: "test",
+  });
 });
 
 client.on("interactionCreate", async (interaction) => {
+  console.log(interaction?.isCommand(), interaction?.isSelectMenu())
+
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
@@ -56,8 +65,60 @@ client.on("interactionCreate", async (interaction) => {
       executeLeaderboardCommand(interaction);
     else if (commandName === "setup") executeSetupCommand(interaction);
     else if (commandName === "loyalty") executeLoyaltyCommand(interaction);
+    else if (commandName === "test") {
+      // if (interaction.isCommand()) {
+      const row = new MessageActionRow().addComponents(
+        new MessageSelectMenu()
+          .setCustomId('select')
+          .setPlaceholder('Nothing selected')
+          .setMinValues(1)
+          .setMaxValues(1)
+          .addOptions([
+            {
+              label: 'Select me',
+              description: 'This is a description',
+              value: 'first_option',
+            },
+            {
+              label: 'You can select me too',
+              description: 'This is also a description',
+              value: 'second_option',
+            },
+            {
+              label: 'I am also an option',
+              description: 'This is a description as well',
+              value: 'third_option',
+            },
+          ]));
+
+      await interaction?.reply({ content: "Pong", components: [row], ephemeral: true });
+
+      // }
+      // else if (interaction?.isSelectMenu()) {
+      //   console.log(32)
+      //   if (interaction.customId === 'select') {
+      //     const selected = interaction?.values[0]
+      //     console.log(selected)
+      //     await interaction?.reply({ content: selected, ephemeral: true })
+      //   }
+      // }
+
+      // const taskCollector = interaction.channel?.createMessageComponentCollector({
+      //   componentType: MessageComponentTypes.SELECT_MENU,
+      // })
+
+      // taskCollector?.on('collect', async(collected) => {
+      //   console.log((collected));
+      // })
+
+
+    }
   } catch (error) {
     console.log(error);
     // TODO capture error on sentry
   }
 });
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
