4046970ec30793a4a53b2abb16320133d51a023b
Aniketh Panchidi <ani.p1995@gmail.com>
2023-05-25 20:59:22 +0530

    port no changed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import bodyParser from "body-parser";
+import nconf from "nconf";
 import express from "express";
 import * as http from "http";
 import cors from "cors";
 import cron from "node-cron";
 import { submissions } from "./controller/zealyBot";
+
 import "./bots/gm";
 import {
   checkInfluencerLike,
@@ -33,6 +35,6 @@ const job = async () => {
 };
 job();
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", nconf.get("PORT") || 5001);
 const port = app.get("port");
 server.listen(port, () => console.log(`Server started on port ${port}`));
