f1e95abaa5c8b9c062e4fc0cb501d2a10ec2ca80
Aniketh Panchidi <ani.p1995@gmail.com>
2024-01-04 16:37:10 +0530

    minor change

--- src/controller/contracts.ts
+++ src/controller/contracts.ts
@@ -5,9 +5,7 @@ import troveManagerABI from "../abis/TroveManager.json";
 import poolABI from "../abis/Pool.json";
 import stabilityPool from "../abis/StabilityPool.json";
 
-const provider = new ethers.JsonRpcProvider(
-  nconf.get("ZKSYNC_RPC_URL_TESTNET")
-);
+const provider = new ethers.JsonRpcProvider(nconf.get("ZKSYNC_RPC_URL"));
 
 export const PoolContract = new ethers.Contract(
   nconf.get("POOL"),
