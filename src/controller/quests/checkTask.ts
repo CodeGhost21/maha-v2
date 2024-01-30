3e41a15049e169df31fc304449e79834321c6517
Aniketh Panchidi <ani.p1995@gmail.com>
2024-01-30 18:26:02 +0530

    try catch method added to task verification

--- src/controller/quests/checkTask.ts
+++ src/controller/quests/checkTask.ts
@@ -5,7 +5,7 @@ import {
 import { checkGuildMember } from "../../output/discord";
 import { points } from "./constants";
 import { assignPoints } from "./assignPoints";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import pythAddresses from "../../addresses/pyth.json";
 
 interface IPythStaker {
@@ -14,105 +14,112 @@ interface IPythStaker {
   evm: string;
 }
 
-export const checkTask = async (req: Request, res: Response) => {
+export const checkTask = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const user = req.user as IWalletUserModel;
+  try {
+    if (req.body.taskId === "discordFollow") {
+      const checkDiscordFollow = await checkGuildMember(user.discordId);
+      user.checked.discordFollow = checkDiscordFollow;
+      await user.save();
 
-  if (req.body.taskId === "discordFollow") {
-    const checkDiscordFollow = await checkGuildMember(user.discordId);
-    user.checked.discordFollow = checkDiscordFollow;
-    await user.save();
-
-    if (checkDiscordFollow) {
-      const task = await assignPoints(
-        user.id,
-        points.discordFollow,
-        "Discord Follower",
-        true,
-        "discordFollow"
-      );
-
-      await task?.execute();
-    }
-  } else if (req.body.taskId === "PythStaker") {
-    console.log(req.body.taskId);
-    const typedAddresses: IPythStaker[] = pythAddresses as IPythStaker[];
-    const pythData = typedAddresses.find(
-      (item) => item.evm === user.walletAddress
-    );
-    console.log(pythData);
-
-    if (pythData) {
-      const stakedAmount = pythData.stakedAmount / 1e6;
-      if (stakedAmount > 0) {
+      if (checkDiscordFollow) {
         const task = await assignPoints(
           user.id,
-          stakedAmount,
-          "Pyth Staker",
+          points.discordFollow,
+          "Discord Follower",
           true,
-          "PythStaker"
+          "discordFollow"
         );
+
         await task?.execute();
       }
+    } else if (req.body.taskId === "PythStaker") {
+      console.log(req.body.taskId);
+      const typedAddresses: IPythStaker[] = pythAddresses as IPythStaker[];
+      const pythData = typedAddresses.find(
+        (item) => item.evm === user.walletAddress
+      );
+      console.log(pythData);
+
+      if (pythData) {
+        const stakedAmount = pythData.stakedAmount / 1e6;
+        if (stakedAmount > 0) {
+          const task = await assignPoints(
+            user.id,
+            stakedAmount,
+            "Pyth Staker",
+            true,
+            "PythStaker"
+          );
+          await task?.execute();
+        }
+      }
     }
+
+    // } else if (req.body.taskId === "twitterFollow" && !user.checked.twitterFollow) {
+    //   await assignPoints(
+    //     user.id,
+    //     points.twitterFollow,
+    //     "Twitter Follower",
+    //     true,
+    //     req.body.taskId
+    //   );
+    //   user.checked.twitterFollow = true;
+    //   await user.save();
+    // } else if (req.body.taskId === "LQTYHolder") {
+    //   if (LQTYHolders.includes(user.walletAddress) && !user.checked.LQTYHolder) {
+    //     user.LQTYHolderChecked = true;
+    //     await assignPoints(
+    //       user.id,
+    //       points.LQTYHolder,
+    //       "LQTY Holder",
+    //       true,
+    //       req.body.taskId
+    //     );
+    //   }
+    // } else if (req.body.taskId === "AAVEStaker" && !user.AAVEStakersChecked) {
+    //   if (AAVEStakers.includes(user.walletAddress)) {
+    //     user.AAVEStakersChecked = true;
+    //     await assignPoints(
+    //       user.id,
+    //       points.AAVEStaker,
+    //       "AAVE Staker",
+    //       true,
+    //       req.body.taskId
+    //     );
+    //   }
+    // } else if (req.body.taskId === "MAHAStaker" && !user.MAHAStakerChecked) {
+    //   if (MAHAStakers.includes(user.walletAddress)) {
+    //     user.MAHAStakerChecked = true;
+    //     await assignPoints(
+    //       user.id,
+    //       points.MAHAStaker,
+    //       "MAHA Staker",
+    //       true,
+    //       req.body.taskId
+    //     );
+    //   }
+    // }
+    // else if (req.body.taskId === "LUSDHolder" && !user.LUSDHolderChecked) {
+    //   if (LUSDHolders.includes(user.walletAddress)) {
+    //     user.LUSDHolderChecked = true;
+    //     await assignPoints(
+    //       user,
+    //       points.LUSDHolder,
+    //       "LUSD Holder",
+    //       true,
+    //       req.body.taskId
+    //     );
+    //   }
+    // }
+
+    const newUser = await WalletUser.findById(user.id);
+    res.json({ success: true, user: newUser });
+  } catch (error) {
+    return next(error);
   }
-
-  // } else if (req.body.taskId === "twitterFollow" && !user.checked.twitterFollow) {
-  //   await assignPoints(
-  //     user.id,
-  //     points.twitterFollow,
-  //     "Twitter Follower",
-  //     true,
-  //     req.body.taskId
-  //   );
-  //   user.checked.twitterFollow = true;
-  //   await user.save();
-  // } else if (req.body.taskId === "LQTYHolder") {
-  //   if (LQTYHolders.includes(user.walletAddress) && !user.checked.LQTYHolder) {
-  //     user.LQTYHolderChecked = true;
-  //     await assignPoints(
-  //       user.id,
-  //       points.LQTYHolder,
-  //       "LQTY Holder",
-  //       true,
-  //       req.body.taskId
-  //     );
-  //   }
-  // } else if (req.body.taskId === "AAVEStaker" && !user.AAVEStakersChecked) {
-  //   if (AAVEStakers.includes(user.walletAddress)) {
-  //     user.AAVEStakersChecked = true;
-  //     await assignPoints(
-  //       user.id,
-  //       points.AAVEStaker,
-  //       "AAVE Staker",
-  //       true,
-  //       req.body.taskId
-  //     );
-  //   }
-  // } else if (req.body.taskId === "MAHAStaker" && !user.MAHAStakerChecked) {
-  //   if (MAHAStakers.includes(user.walletAddress)) {
-  //     user.MAHAStakerChecked = true;
-  //     await assignPoints(
-  //       user.id,
-  //       points.MAHAStaker,
-  //       "MAHA Staker",
-  //       true,
-  //       req.body.taskId
-  //     );
-  //   }
-  // }
-  // else if (req.body.taskId === "LUSDHolder" && !user.LUSDHolderChecked) {
-  //   if (LUSDHolders.includes(user.walletAddress)) {
-  //     user.LUSDHolderChecked = true;
-  //     await assignPoints(
-  //       user,
-  //       points.LUSDHolder,
-  //       "LUSD Holder",
-  //       true,
-  //       req.body.taskId
-  //     );
-  //   }
-  // }
-
-  const newUser = await WalletUser.findById(user.id);
-  res.json({ success: true, user: newUser });
 };
