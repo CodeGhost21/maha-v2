ecb4068ec1594fe5c855dd5f8885e586fc3c5083
Deadshot Ryker <ryker@zerolend.xyz>
2024-05-03 07:42:23 +0530

    update code

--- src/services.ts
+++ src/services.ts
@@ -40,30 +40,35 @@ cron.schedule("0 15 * * 6", async () => {
   await updateMantaPoints();
 });
 
-// cron.schedule("0 1 * * * *", async () => {
-//   console.log("running zksyn lp points every day at 1 am");
-//   await zksyncCron();
-// });
+cron.schedule("0 1 * * * *", async () => {
+  console.log("running zksyn lp points every day at 1 am");
+  await zksyncCron();
+});
 
-// cron.schedule("0 2 * * * *", async () => {
-//   console.log("running manta lp points every day at 2 am");
-//   await mantaCron();
-// });
+cron.schedule("0 2 * * * *", async () => {
+  console.log("running manta lp points every day at 2 am");
+  await mantaCron();
+});
 
-// cron.schedule("0 3 * * * *", async () => {
-//   console.log("running blast lp points every day at 3 am");
-//   await blastCron();
-// });
+cron.schedule("0 3 * * * *", async () => {
+  console.log("running blast lp points every day at 3 am");
+  await blastCron();
+});
 
-// cron.schedule("0 4 * * * *", async () => {
-//   console.log("running ethereumLrt lp points every day at 4 am");
-//   await ethereumLrtCron();
-// });
+cron.schedule("0 4 * * * *", async () => {
+  console.log("running ethereumLrt lp points every day at 4 am");
+  await ethereumLrtCron();
+});
 
-// cron.schedule("0 5 * * * *", async () => {
-//   console.log("running linea lp points every day at 5 am");
-//   await lineaCron();
-// });
+cron.schedule("0 5 * * * *", async () => {
+  console.log("running linea lp points every day at 5 am");
+  await lineaCron();
+});
+
+cron.schedule("0 6 * * * *", async () => {
+  console.log("running linea lp points every day at 5 am");
+  await xLayerCron();
+});
 
 // cron.schedule("0 6 * * * *", async () => {
 //   console.log("running xLayer lp points every day at 6 am");
