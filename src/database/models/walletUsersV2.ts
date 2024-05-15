dc424048d6329fb711690412a95a1e99904d1d01
Ghost Scripter <ghostscripter@zerolend.xyz>
2024-05-15 12:09:47 +0530

    minor change

--- src/database/models/walletUsersV2.ts
+++ src/database/models/walletUsersV2.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { WalletUserSchema } from "../schema/walletUser";
+import { WalletUserSchemaV2 } from "../schema/walletUserV2";
 import { IWalletUser } from "../interface/walletUser/walletUser";
 
 export type IWalletUserModel = IWalletUser & mongoose.Document;
 export const WalletUserV2 = mongoose.model<IWalletUserModel>(
   "WalletUserV2",
-  WalletUserSchema
+  WalletUserSchemaV2
 );
