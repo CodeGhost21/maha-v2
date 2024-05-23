9dbdeb19c770b1d71f17f90258c4d3979ff7a6c4
Ghost Scripter <ghostscripter@zerolend.xyz>
2024-05-23 13:17:32 +0530

    legacy api's added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import discord from "./discord";
 import user from "./user";
+import userLegacy from "./userLegacy";
 import twitter from "./twitter";
 import blast from "./blast";
 import HttpError from "../errors/HttpError";
@@ -24,6 +25,7 @@ router.get("/leaderBoard", getLeaderBoard);
 router.use("/discord", discord);
 router.use("/twitter", twitter);
 router.use("/user", user);
+router.use("/userLegacy", userLegacy);
 router.use("/blast", blast);
 router.get("/referral/openBlock", getOpensBlockData);
 
