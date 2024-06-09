1befbc0b79ce8f3da431033fcaa508c4ca9824f9
Ghost Scripter <ghostscripter@zerolend.xyz>
2024-06-09 13:39:23 +0530

    cron schedule change

--- src/services.ts
+++ src/services.ts
@@ -21,7 +21,7 @@ console.log("starting");
 
 // -------------  Update LP Rate  -----------------
 cron.schedule(
-  "30 00 * * *",
+  "00 14 * * *",
   async () => {
     addToQueue(async () => {
       console.log("running lp points every day at 12:30 am");
