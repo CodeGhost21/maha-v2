3d9b78a38d2936a98d7d77f9fc9ac4f5b4839558
Ghost Scripter <ghostscripter@zerolend.xyz>
2024-07-25 12:39:40 +0530

    minor change

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,7 @@ import routes from "./routes";
 import session from "express-session";
 import { totalPoints } from "./cron/totalPoints";
 import "./strategies";
-import buyNotifBot from "./scripts/buyNotifBot";
-
+import buyNotifBot, { getMarketCap } from "./scripts/buyNotifBot";
 
 const app = express();
 const server = new http.Server(app);
@@ -44,7 +43,7 @@ server.listen(port, () => console.log(`server started on port ${port}`));
 
 // notification for $ZERO buy
 buyNotifBot()
-
+getMarketCap()
 
 // setup LB cache
 cron.schedule("*/10 * * * *", async () => {
@@ -57,4 +56,5 @@ cron.schedule("*/10 * * * *", async () => {
 cron.schedule("0 * * * *", async () => {
   console.log("updating totalPoints every hour");
   await totalPoints();
+  await getMarketCap()
 });
